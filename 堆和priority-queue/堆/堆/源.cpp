7c5f98993129e34924716e77732baf0f8b0fd2a9
Syt <valiant_0714@qq.com>
2025-09-23 08:49:59 +0800

    9.23

--- 堆和priority-queue/堆/堆/源.cpp
+++ 堆和priority-queue/堆/堆/源.cpp
@@ -19,17 +19,21 @@ void up(int child) {
 
 void down(int parent) {
 	int child = parent * 2;
-	while (child) {
+	while (child <= n) {
 		if (child <= n) {
 			if (child + 1 <= n && heap[child + 1] < heap[child]) child++;
 
-			if (heap[child], heap[parent]) return;
+			if (heap[child]>= heap[parent]) return;
 			swap(heap[child], heap[parent]);
 			parent = child;
 			child = parent * 2;
 		}
 	}
 
+	void push(int x) {
+		heap[++n] = x;
+		up(n);
+	}
 	void pop() {
 		swap(heap[1], heap[n]);
 		n--;
